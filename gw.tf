ab5f5009d53abb968a540ead96f75deabafb5030
vinod sjml <vinsjml@gmail.com>
2024-02-12 11:11:04 +0530

    push

--- gw.tf
+++ gw.tf
@@ -24,5 +24,5 @@ resource "aws_nat_gateway" "ngw" {
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.igw, aws_eip.ngw_ip]
+  depends_on = [aws_internet_gateway.igw, aws_eip.ngw_eip]
 }
