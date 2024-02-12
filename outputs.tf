c2b4ebcc4627e36324c5738ba02bf0c2fcbebd59
vinod sjml <vinsjml@gmail.com>
2024-02-12 19:39:20 +0530

    push

---
+++ outputs.tf
@@ -0,0 +1,31 @@
+output "VPC_CIDR" {
+  value = aws_vpc.main.VPC_CIDR
+}
+
+output "VPC_ID" {
+  value = aws_vpc.main.id
+}
+
+output "PUBLIC_SUBNET_CIDR" {
+  value = aws_subnet.public_subnet.*.cidr_block
+}
+
+output "PRIVATE_SUBNET_CIDR" {
+  value = aws_subnet.private_subnet.*.cidr_block
+}
+
+output "PUBLIC_SUBNET_IDS" {
+  value = aws_subnet.public_subnet.*.id
+}
+
+output "PRIVATE_SUBNET_IDS" {
+  value = aws_subnet.private_subnet.*.id
+}
+
+output "DEFAULT_VPC_CIDR" {
+  value = var.DEFAULT_VPC_CIDR
+}
+
+output "DEFAULT_VPC_ID" {
+  value = var.DEFAULT_VPC_ID
+}
