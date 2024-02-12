9dc287bbf29d6f2939d1059abf9270b970d0b892
vinod sjml <vinsjml@gmail.com>
2024-02-12 10:18:44 +0530

    push

---
+++ gw.tf
@@ -0,0 +1,28 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "roboshop-${var.ENV}-igw"
+  }
+}
+
+resource "aws_eip" "ngw_eip" {
+  vpc = true
+
+  tags = {
+    Name = "roboshop-${var.ENV}-ngw-eip"
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw_eip.id
+  subnet_id     = aws_subnet.public_subnet.*.id[0]
+
+  tags = {
+    Name = "nat gw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw, aws_eip.ngw_ip]
+}
