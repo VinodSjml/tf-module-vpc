7c3544a94c2c201a2a1ab1e842d2326d7895ffbd
vinod sjml <vinsjml@gmail.com>
2024-02-12 10:55:23 +0530

    push

--- rt.tf
+++ rt.tf
@@ -45,3 +45,11 @@ resource "aws_route_table_association" "private_subnet_rt_association" {
   subnet_id = element(aws_subnet.private_subnet.*.id,count.index)
   route_table_id = aws_route_table.private_rt.id
 }
+
+resource "aws_route" "default_vpc_route" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+
+  depends_on = [aws_vpc_peering_connection.peer]
+}
